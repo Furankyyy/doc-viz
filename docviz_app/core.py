991f52c2fe59b663566fa66194ac742ed2971cd7
Furankyyy <furankyyy@yahoo.com>
2020-12-28 05:01:23 -0800

    f

--- docviz_app/core.py
+++ docviz_app/core.py
@@ -8,13 +8,9 @@ from rq import Queue
 
 server = flask.Flask(__name__)
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
+app = dash.Dash(server=server, external_stylesheets=external_stylesheets,  serve_locally=False)
 
 # redis connection and RQ queue. use redistogo service when dpeloying to Heroku
 redis_url = os.getenv("REDISTOGO_URL", "redis://localhost:6379")
 conn = redis.from_url(redis_url)
 queue = Queue(connection=conn)
-
-
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
