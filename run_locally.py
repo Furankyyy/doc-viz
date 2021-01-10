385bec7f773e47f704c98ac321ccdc439133938b
Furankyyy <furankyyy@yahoo.com>
2021-01-10 01:07:15 -0800

    c

--- run_locally.py
+++ run_locally.py
@@ -3,5 +3,5 @@ import os
 from docviz_app import app
 
 if __name__ == "__main__":
-    #app.run_server(debug=True, host=os.getenv("APP_HOST", "127.0.0.1"))
-    app.run_server(debug=False, host='0.0.0.0', port='8080')
+    app.run_server(debug=True, host=os.getenv("APP_HOST", "127.0.0.1"))
+    #app.run_server(debug=False, host='0.0.0.0', port='8080')
